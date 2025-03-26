024e2f910b61ad29e9fa9e44040f182943a56db9
Raphico2 <raphcohen@hotmail.fr>
2025-03-26 06:45:13 -0300

    Bureaucrat class updated

--- ex02/Bureaucrat.cpp
+++ ex02/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: raphco <raphco@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 22:07:56 by tcohen            #+#    #+#             */
-/*   Updated: 2025/03/26 06:19:01 by raphco           ###   ########.fr       */
+/*   Updated: 2025/03/26 06:43:59 by raphco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,13 +86,27 @@ void Bureaucrat::signForm(AForm &form)
     try
     {
         form.beSigned(*this);
+        std::cout << "Bureraucrat " << this->_Name << " signed form " << form.GetName() << '\n';
     }
     catch(const std::exception& e)
     {
         std::cerr << "Bureraucrat " << this->_Name << " couldn't sign form " << form.GetName() << " because " << e.what() << '\n';
+    }    
+}
+
+void Bureaucrat::executeForm(AForm const & form)
+{
+    try
+    {
+        form.execute(*this);
+        std::cout << "Bureaucrat " << this->_Name << "succesfully executed form " << form.GetName() << std::endl;
     }
-    if (form.GetSigned())
-        std::cout << "Bureraucrat " << this->_Name << " signed form " << form.GetName() << '\n';
+    catch(const std::exception& e)
+    {
+        std::cerr << "Form " << form.GetName() << " cannot be executed by " << this->_Name << 
+                " because "<< e.what() << '\n';
+    }
+
     
 }
 
--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -6,7 +6,18 @@
 /*   By: raphco <raphco@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 00:37:23 by raphco            #+#    #+#             */
-/*   Updated: 2025/03/25 00:37:58 by raphco           ###   ########.fr       */
+/*   Updated: 2025/03/26 06:44:27 by raphco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+    private: 
+        std::string _target;
+        virtual void DoYourThing();
+    public:
+        //member functiuns
+
+};

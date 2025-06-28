216afa7dd04787f45916306dc3907afcb656d7dd
TheoCo27 <theocohen27@gmail.com>
2025-06-28 02:30:04 +0200

    ex02 done need to check chmod for shruberry

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -3,10 +3,71 @@
 /*                                                        :::      ::::::::   */
 /*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raphco <raphco@student.42.fr>              +#+  +:+       +#+        */
+/*   By: theog <theog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 00:37:36 by raphco            #+#    #+#             */
-/*   Updated: 2025/03/25 00:37:45 by raphco           ###   ########.fr       */
+/*   Updated: 2025/06/28 02:21:00 by theog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137), _target("default")
+{
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm("ShrubberyCreationForm", 145, 137), _target(target)
+{
+
+}
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm &copy)
+{
+    *this = copy;
+}
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(ShrubberyCreationForm &copy)
+{
+    if (this == &copy)
+        return(*this);
+    this->SetSigned(copy.GetSigned());
+    return (*this);
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+
+}
+
+void ShrubberyCreationForm::DoYourThing() const
+{
+    std::ofstream outfile((_target + std::string("_shrubbery")).c_str());
+
+	if (outfile)
+	{
+        outfile << "        &&& &&  & &&" << std::endl;
+        outfile << "      && &/|&|& ()|/ @, &&" << std::endl;
+        outfile << "      &\\/(/&/&||/& /_/)_&/_&" << std::endl;
+        outfile << "   &() &\\/&|()|/&\\/ '%\" & ()" << std::endl;
+        outfile << "  &_\\_&&_\\ |& |&&/&__%_/_& &&" << std::endl;
+        outfile << "&&   && & &| &| /& & % ()& /&&" << std::endl;
+        outfile << " ()&_---()&\\&\\|&&-&&--%---()~" << std::endl;
+        outfile << "     &&     \\|||" << std::endl;
+        outfile << "             |||" << std::endl;
+        outfile << "             |||" << std::endl;
+        outfile << "             |||" << std::endl;
+        outfile << "       , -=-~  .-^- _" << std::endl;
+		outfile.close();
+	}
+	else
+		throw (FileException());
+}
+
+std::string ShrubberyCreationForm::get_target(void)
+{
+    return (_target);
+}
+const char* ShrubberyCreationForm::FileException::what() const throw()
+{
+    return ("Couldn't open and write the file");
+}
+    
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,14 @@
 /*   By: theog <theog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 22:08:05 by tcohen            #+#    #+#             */
-/*   Updated: 2025/06/28 01:48:27 by theog            ###   ########.fr       */
+/*   Updated: 2025/06/28 02:22:11 by theog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main()
 {
@@ -21,10 +22,13 @@ int main()
     Bureaucrat Scott;
     PresidentialPardonForm f1;
     RobotomyRequestForm f2("chips");
+    ShrubberyCreationForm f3("blue");
+
     try
     {
         Scott.signForm(f1);
         Scott.signForm(f2);
+        Scott.signForm(f3);
     }
     catch (std::exception &e)
     {
@@ -34,6 +38,7 @@ int main()
     {
         Scott.executeForm(f1);
         Scott.executeForm(f2);
+        Scott.executeForm(f3);
     }
     catch (std::exception &e)
     {
@@ -44,6 +49,15 @@ int main()
     {
         Pilgrim.signForm(f1);
         Pilgrim.executeForm(f1);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        Pilgrim.signForm(f3);
+        Pilgrim.executeForm(f3);
     }
     catch (std::exception &e)
     {
@@ -64,4 +78,5 @@ int main()
 
     std::cout << f1;
     std::cout << f2;
+    std::cout << f3;
 }
